ad0273aa78db0a4489ea26f939294845fcc3923c
scastillo <sebastian.castillo@cenuage.com>
2011-05-14 12:56:22 -0500

    Fixed key is_authenticated method

--- auth.py
+++ auth.py
@@ -5,15 +5,17 @@ import logging
 
 class ApiKeyAuthentication( object ):
     def is_authenticated( self, request ):
+        logging.error("fff")
         auth_string = request.META.get( 'HTTP_AUTHORIZATION' )
+        logging.error("auth_string %s" % auth_string)
         if not auth_string:
             return False
         try:
-            key = ApiKey.objects.get( key=auth_string )
+            key = ApiKey.objects.get(key=auth_string)
+            logging.error("key: %s" % key)
             request.user = key.user
             return True
         except:
-            raise
             request.user = AnonymousUser( )
             return False
 
