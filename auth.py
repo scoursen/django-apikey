21d8f940ffcdbd553bb4639a42e5482c30d97fac
scastillo <sebastian.castillo@cenuage.com>
2011-05-14 12:36:04 -0500

    Removed ugly faulty django-auth support

--- auth.py
+++ auth.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import *
-from django.shortcuts import render_to_response
 from key.models import ApiKey
 import logging
 
@@ -23,26 +22,3 @@ class ApiKeyAuthentication( object ):
         resp['WWW-Authenticate'] = 'KeyBasedAuthentication realm="API"'
         resp.status_code = 401
         return resp
-
-    """
-    Support for usage as a regular Django authentication backend.
-    You probably don't want to do this, but it's included for completeness.
-    """
-    supports_object_permissions = False
-    supports_anonymous_user = False
-    supports_inactive_user = False
-
-    def get_user( self, user_id ):
-        """user_id is really the API key"""
-        try:
-            key = ApiKey.objects.get( key=user_id )
-            return key.user
-        except Key.DoesNotExist:
-            return None
-
-    def is_authenticated( self, username=None, password=None ):
-        try:
-            key = ApiKey.objects.get( key=username )
-            return key.user
-        except Key.DoesNotExist:
-            return None
