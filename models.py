7082ecda7eb4e3c4b1c41f42d8865b65a0e85433
scastillo <sebastian.castillo@cenuage.com>
2011-05-14 12:14:27 -0500

    Fixed key_size bug, removed unused imports and aplied pep8 style guidelines

--- models.py
+++ models.py
@@ -1,41 +1,38 @@
 from django.db import models
-from djangotoolbox import fields
 from django.contrib.auth.models import User
 from django.contrib import admin
 from django.conf import settings
 from datetime import datetime
-import collections
 import logging
-import pickle
 
 try:
     key_size = settings.API_KEY_SIZE
 except:
     key_size = 16
 
-class ApiKey( models.Model ):
-    user = models.ForeignKey( User, related_name='keys' )
-    key = models.CharField( max_length=key_size, unique=True )
-    logged_ip = models.IPAddressField( null=True )
-    last_used = models.DateTimeField( default=datetime.utcnow )
-    def __unicode__( self ):
+class ApiKey(models.Model):
+    user = models.ForeignKey(User, related_name='keys')
+    key = models.CharField(max_length=key_size, unique=True)
+    logged_ip = models.IPAddressField(null=True)
+    last_used = models.DateTimeField(default=datetime.utcnow)
+    def __unicode__(self):
         return self.key
 
-def generate_unique_api_key( user ):
-    key = User.objects.make_random_password( length=key_size )
-    while ApiKey.objects.filter( key__exact=key ).count( ):
-        key = User.objects.make_random_password( length=ks )
-    k = ApiKey( user = user, key=key )
-    k.save( )
+def generate_unique_api_key(user):
+    key = User.objects.make_random_password(length=key_size)
+    while ApiKey.objects.filter(key__exact=key).count():
+        key = User.objects.make_random_password(length=key_size)
+    k = ApiKey(user=user, key=key)
+    k.save()
     return k
-        
-if User.objects.filter( username='nqdq_system' ):
-    if not User.objects.get( username='nqdq_system' ).keys.all( ):
-        u = User.objects.get( username='nqdq_system' )
-        logging.info( 'Generating API key for nqdq_system' )
-        k = generate_unique_api_key( u )
-        u.save( )
-    else:
-        logging.info( 'Not generating API key' )
 
-admin.site.register( ApiKey )
+if User.objects.filter(username='nqdq_system'):
+    if not User.objects.get(username='nqdq_system').keys.all():
+        u = User.objects.get(username='nqdq_system')
+        logging.info('Generating API key for nqdq_system')
+        k = generate_unique_api_key(u)
+        u.save()
+    else:
+        logging.info('Not generating API key')
+
+admin.site.register(ApiKey)
