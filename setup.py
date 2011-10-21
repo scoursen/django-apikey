392f8446bb4cd93ca8a5d19d173c6a4a8d293eb9
Stephen Coursen <smcoursen@gmail.com>
2011-10-21 14:09:56 -0600

    Added license and url

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='django-apikey',
+setup(name='django-key',
       version='0.5.0',
       description='Simple, straight forward API key for django-piston.',
       author='Steve Coursen',
-      author_email='me@stevecoursen.com',
+      author_email='smcoursen@gmail.com',
       packages=find_packages(),
+      license="BSD",
+      url="https://github.com/scoursen/django-apikey",
       install_requires=['setuptools',],
       include_package_data=True,
       setup_requires=['setuptools_git',],
