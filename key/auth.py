636d4113fd020c41e2e3c18f1d17d5c8e70dabff
Stephen Coursen <smcoursen@gmail.com>
2011-11-29 11:06:44 -0700

    Refactored caching out of standard auth module, now caching is handled by an optional subclass

--- key/auth.py
+++ key/auth.py
@@ -18,22 +18,44 @@ class ApiKeyAuthentication(object):
         auth_string = request.META.get(auth_header)
         if not auth_string:
             return False
+        key = ApiKey.objects.get(key=auth_string)
+        request.user = key.profile.user
+        if not key.profile.user.has_perm('key.can_use_api'):
+            return False
+        key.login(request.META.get('REMOTE_ADDR'))
+        return True
+
+    def challenge(self):
+        auth_header = getattr(settings, 'AUTH_HEADER')
+        resp = HttpResponse('Authorization Required')
+        resp['WWW-Authenticate'] = 'KeyBasedAuthentication realm="API"'       
+        resp[auth_header] = 'Key Needed'
+        resp.status_code = 401
+        return resp
+
+
+class CachingAuthentication(ApiKeyAuthentication):
+    def is_authenticated(self, request):
+        auth_header = getattr(settings, 'AUTH_HEADER')
+        auth_header = 'HTTP_%s' % (auth_header.upper().replace('-', '_'))
+        auth_string = request.META.get(auth_header)
+        if not auth_string:
+            return False
         try:
             user = cache.get_cache('default').get(_key_cache_key(auth_string))
             if user:
                 request.user = user
             else:
-                key = ApiKey.objects.get(key=auth_string)
-                request.user = key.profile.user
-                cache.get_cache('default').set(_key_cache_key(key.key), request.user)
-                if not key.profile.user.has_perm('key.can_use_api'):
-                    return False                
-                key.login(request.META.get('REMOTE_ADDR'))
+                rv = super(CachingAuthentication, self).is_authenticated(request)
+                if rv:
+                    key = ApiKey.objects.get(key=auth_string)
+                    cache.get_cache('default').set(_key_cache_key(key.key),
+                                                   request.user)
         except ApiKey.DoesNotExist:
             return False
         request.key = auth_string
         return True
-        
+
     def challenge(self):
         auth_header = getattr(settings, 'AUTH_HEADER')
         resp = HttpResponse('Authorization Required')
